4e321348d7f1ccfb4274304a6dbbee63be6c1b3e
Ladebeze66 <fgras.ca@gmail.com>
2025-02-22 16:30:01 +0100

    docker2

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import os
 sys.path.append(os.path.abspath("src"))
 
 from src.training_model import train_model
-from src.prediction import predict_price  # ✅ Peut maintenant être importé dès le début
-from src.gui import show_gui  # ✅ Import propre et structuré
+from src.prediction import predict_price  # Peut maintenant être importé dès le début
+from src.gui import show_gui  # Import propre et structuré
 
 def main():
     print("Bienvenue dans le modèle de prédiction de prix de voiture !")
-    print("🔄 Le modèle va être entraîné...")
+    print("Le modèle va être entraîné...")
 
     # Entraîner le modèle
     train_model()
